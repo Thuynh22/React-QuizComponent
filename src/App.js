51bc207ea0cdfb8dafd9c565c32c39de9ed7d28d
Tu Huynh <tuhuynh1996@gmail.com>
2019-02-13 11:54:56 -0600

    .gitignore fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Quiz from './Quiz.js'
+import QuizQuestion from './QuizQuestion.js'
 import './App.css'
+
 
 class App extends Component {
   render() {
